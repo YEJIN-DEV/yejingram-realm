aecdd1bc2cdf8f5ff1117d8cf0722581c0f0b87c
AuroraCaelum <ddugi1214@naver.com>
2025-12-10 03:21:46 +0900

    fix: use vercel env to support different environment

--- src/main.tsx
+++ src/main.tsx
@@ -22,7 +22,7 @@ window.addEventListener('message', (event) => {
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.ap-northeast-2.amazonaws.com/ap-northeast-2_qpXc0tRPJ",
   client_id: "3mjaiv26pdraeb0erjj5am92ve",
-  redirect_uri: import.meta.env.DEV ? "http://localhost:5173" : "https://realm.yejingram.com",
+  redirect_uri: import.meta.env.DEV ? "http://localhost:5173" : import.meta.env.REDIRECT_URI,
   response_type: "code",
   scope: "email openid profile",
 };
